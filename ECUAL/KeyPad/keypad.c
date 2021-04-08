360b1c7b816e17a02beba27d2090332d22ee4baa
MohamedSalah3 <mhmd.ahmd789@gmail.com>
2021-04-08 17:48:58 +0200

    keypad

--- ECUAL/KeyPad/keypad.c
+++ ECUAL/KeyPad/keypad.c
@@ -1,1 +1,28 @@
 /*KeyPad.c*/
+
+
+
+ERROR_STATUS KeyPad_Init()
+{
+
+
+}
+
+
+
+
+
+ERROR_STATUS KeyPad_Get_Status()
+{
+
+
+}
+
+
+
+
+ERROR_STATUS KeyPad_Update()
+{
+
+
+}
--- ECUAL/KeyPad/keypad_config.h
+++ ECUAL/KeyPad/keypad_config.h
@@ -1,1 +1,16 @@
 /*keypad_config.h*/
+
+
+typedef struct KeyPad_matrix_str{
+	uint8_t u8_Channel_Number;
+	uint8_t u8_Prescaler;
+	uint8_t u8_voltage_refrences;
+	uint8_t u8_ten_bit_arrangment;
+	uint8_t u8_polling_interrupt;
+}KeyPad_matrix_str;
+
+
+
+
+
+extern KeyPad_matrix_str Keypad;
